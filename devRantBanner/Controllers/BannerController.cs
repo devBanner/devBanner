fdd6a39714786f6c110f23f5051e01a00253c07e
Kim-Maximilian Nürnberger <git@kim-nuernberger.eu>
2017-11-27 22:58:41 +0100

    Changed endpoints from /api/ to /generate/

--- devRantBanner/Controllers/BannerController.cs
+++ devRantBanner/Controllers/BannerController.cs
@@ -9,7 +9,7 @@ using System.Net.Http;
 
 namespace devBanner.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("generate/[controller]")]
     public class BannerController : Controller
     {
         private string DevrantAvatarBaseURL { get; set; } = "https://avatars.devrant.com";
